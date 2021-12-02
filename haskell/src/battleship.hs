23ba04792a0970611760660c96e99f1b4c67084d
Nick Kao <kobenkao@gmail.com>
2021-12-02 11:54:15 -0800

    additions to the notes trying to get to compile

--- haskell/src/battleship.hs
+++ haskell/src/battleship.hs
@@ -4,9 +4,9 @@
 -- location of ships
 -- location of hits on your ships
 
--- Other grid is opponent battleships: 
+-- Other grid is opponent battleships:
 -- location of sunk ships
--- location of hits of your guesses 
+-- location of hits of your guesses
 -- location of misses of your guesses
 
 -- ships cover squares on the grid
@@ -39,3 +39,30 @@
 -- if you hit then it also affects opponents own grid - adds to opponent's YourGrid misses
 
 -- 1 init server game state and 1 init client game state
+
+-- module Lib ( BattleshipState ) where
+--   Grid =
+--   type Coord = (Int, Int)
+
+data Ship = Ship [GridSquare]
+-- Well formed ship is straight and connected
+-- straight
+    -- Either XCoord or YCoord is all the same
+-- connected
+    -- Whichever is not the same must be able to be sorted
+    -- and then ascending or descending without break
+    -- e.g. C D E
+    -- 5 6 7
+-- E.g. Ship [Coord A 3, Coord A 5, Coord A 4]
+-- Sorted by the changing type of YCoord
+-- Ship [Coord A 3, Coord A 4, Coord A 5]
+-- X Cord is repeating and YCord is contiguous (no repeats)
+
+-- no repeats maybe a set would be better
+
+data GridSquare = GridSquare XCoord YCoord
+
+data XCoord = A | B | C | D | E | F | G | H | I | J
+  deriving (Bounded, Enum, Show)
+data YCoord = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+  deriving (Bounded, Enum, Show)
