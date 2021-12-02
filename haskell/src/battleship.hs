80108345122139c88db397ae1ceb85cafee57b0e
UBC Student <smethven@students.cs.ubc.ca>
2021-12-02 12:23:00 -0800

    set up initial server grid state

--- haskell/src/battleship.hs
+++ haskell/src/battleship.hs
@@ -1,68 +1,71 @@
--- Battleship are two 10*10 grids, x-axis: A-J, y-axis: 1-10
+-- Battleship are two 10*10 grids, x-axis: A-J, y-axis: N1-N10
 
 -- One grid is your own battleships described by:
 -- location of ships
 -- location of hits on your ships
 
--- Other grid is opponent battleships:
+-- Other grid is opponent battleships: 
 -- location of sunk ships
--- location of hits of your guesses
+-- location of hits of your guesses 
 -- location of misses of your guesses
 
 -- ships cover squares on the grid
--- how many ships and of what size?
 -- 1 carrier: 5 squares long
 -- 1 battleship: 4 squares long
 -- 1 cruiser: 3 squares long
 -- 1 submarine: 3 squares long
 -- 1 destroyer: 2 squares long
 
--- data BattleshipState = BattleshipState YourGrid OpponentGrid
+data BattleshipState = BattleshipState YourGrid OpponentGrid
+    deriving (Eq, Ord, Read, Show)
 
 -- YourGrid [Ships] [Hits]
--- data YourGrid = YourGrid [Ship] [GridSquare]
+data YourGrid = YourGrid [Ship] [GridSquare]
+    deriving (Eq, Ord, Read, Show)
 
 -- OpponentGrid [SunkShips] [Hits] [Misses]
--- data OpponentGrid = OpponentGrid [Ship] [GridSquare] [GridSquare]
+data OpponentGrid = OpponentGrid [Ship] [GridSquare] [GridSquare]
+    deriving (Eq, Ord, Read, Show)
 
 -- a well-formed ship has all gridsquares in a line
--- data Ship = Ship [GridSquare]
+data Ship = Ship [GridSquare]
+    deriving (Eq, Ord, Read, Show)
 
--- data GridSquare = GridSquare XCoord YCoord
-
--- data XCoord = A | B | C | D | E | F | G | H | I | J
---  deriving (Bounded, Enum, Show)
--- data YCoord = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
---  deriving (Bounded, Enum, Show)
+data GridSquare = GridSquare XCoord YCoord
+    deriving (Eq, Ord, Read, Show)
+data XCoord = A | B | C | D | E | F | G | H | I | J
+    deriving (Eq, Ord, Read, Bounded, Enum, Show)
+data YCoord = N1 | N2 | N3 | N4 | N5 | N6 | N7 | N8 | N9 | N10
+    deriving (Eq, Ord, Read, Bounded, Enum, Show)
 
 -- your move involves making a guess which would add to either hits or misses list of OpponentGrid, and if it hits then possibly adds to SunkShips list
 -- if you hit then it also affects opponents own grid - adds to opponent's YourGrid misses
 
 -- 1 init server game state and 1 init client game state
 
--- module Lib ( BattleshipState ) where
---   Grid =
---   type Coord = (Int, Int)
+initBattleshipServerState :: BattleshipState
+initBattleshipServerState = BattleshipState
 
-data Ship = Ship [GridSquare]
--- Well formed ship is straight and connected
--- straight
-    -- Either XCoord or YCoord is all the same
--- connected
-    -- Whichever is not the same must be able to be sorted
-    -- and then ascending or descending without break
-    -- e.g. C D E
-    -- 5 6 7
--- E.g. Ship [Coord A 3, Coord A 5, Coord A 4]
--- Sorted by the changing type of YCoord
--- Ship [Coord A 3, Coord A 4, Coord A 5]
--- X Cord is repeating and YCord is contiguous (no repeats)
+initYourGridServer :: YourGrid
+initYourGridServer = YourGrid initShipsServer []
 
--- no repeats maybe a set would be better
+initShipsServer :: [Ship]
+initShipsServer = [initCarrierServer, initBattleshipServer, initCruiserServer, initSubmarineServer, initDestroyerServer]
 
-data GridSquare = GridSquare XCoord YCoord
+initCarrierServer :: Ship
+initCarrierServer = Ship [(GridSquare B N2), (GridSquare C N2), (GridSquare D N2), (GridSquare E N2), (GridSquare F N2)]
 
-data XCoord = A | B | C | D | E | F | G | H | I | J
-  deriving (Bounded, Enum, Show)
-data YCoord = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
-  deriving (Bounded, Enum, Show)
+initBattleshipServer :: Ship
+initBattleshipServer = Ship [(GridSquare I N2), (GridSquare I N3), (GridSquare I N4), (GridSquare I N5)]
+
+initCruiserServer :: Ship
+initCruiserServer = Ship [(GridSquare C N4), (GridSquare D N4), (GridSquare E N4)]
+
+initSubmarineServer :: Ship
+initSubmarineServer = Ship [(GridSquare A N7), (GridSquare A N8), (GridSquare A N9)]
+
+initDestroyerServer :: Ship
+initDestroyerServer = Ship [(GridSquare H N10), (GridSquare I N10)]
+
+initOpponentGridServer :: OpponentGrid
+initOpponentGridServer = OpponentGrid [] [] []
