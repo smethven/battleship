c951fbf9429cf1eb7b872cae161358acaa32c7bd
ruchitp <ruchitpalrecha@hotmail.com>
2021-12-05 15:28:02 -0800

    Fixed bugs from POC

--- haskell/src/Server.hs
+++ haskell/src/Server.hs
@@ -1,47 +1,54 @@
-module Server ( createServer ) where
+module Server (createServer) where
 
+import qualified Control.Exception as E
+import qualified Data.ByteString.Char8 as C
 import Network.Socket
 import Network.Socket.ByteString (recv, sendAll)
-import qualified Data.ByteString.Char8 as C
-import qualified Control.Exception as E
 
 {- server socket creation adapted from https://hackage.haskell.org/package/network-3.1.2.5/docs/Network-Socket.html -}
 
 resolve :: IO AddrInfo
 resolve = do
-    let hints = Just $ defaultHints { addrFlags = [AI_PASSIVE, AI_NUMERICSERV], addrSocketType = Stream }
-    head <$> getAddrInfo hints Nothing (Just "3000")
+  let hints = Just $ defaultHints {addrFlags = [AI_PASSIVE, AI_NUMERICSERV], addrSocketType = Stream}
+  head <$> getAddrInfo hints Nothing (Just "3000")
 
 openSocket :: AddrInfo -> IO Socket
 openSocket addr = socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
 
 open :: AddrInfo -> IO Socket
-open addr = E.bracketOnError (openSocket addr) close (\sock -> do
-    setSocketOption sock ReuseAddr 1
-    withFdSocket sock setCloseOnExecIfNeeded
-    bind sock $ addrAddress addr
-    -- putStrLn "Bound to port"
-    listen sock 1
-    -- putStrLn "Listening..."
-    return sock
+open addr =
+  E.bracketOnError
+    (openSocket addr)
+    close
+    ( \sock -> do
+        setSocketOption sock ReuseAddr 1
+        withFdSocket sock setCloseOnExecIfNeeded
+        bind sock $ addrAddress addr
+        -- putStrLn "Bound to port"
+        listen sock 1
+        -- putStrLn "Listening..."
+        return sock
     )
 
 acceptConn :: Socket -> IO ()
-acceptConn sock = E.bracketOnError (accept sock) (close . fst) (\connection _peer -> do
-    -- putStrLn "Connected to client!"
-    sendAll connection (C.pack "Hello World")
+acceptConn sock =
+  E.bracketOnError
+    (accept sock)
+    (close . fst)
+    ( \(connection, _peer) -> do
+        -- putStrLn "Connected to client!"
+        sendAll connection (C.pack "Hello World")
     )
-
 
 createSocket :: IO ()
 createSocket = do
-    -- putStrLn "!!! To test proof of concept: !!!"
-    -- putStrLn "Run on the CS department Linux servers or a Mac, and on the same server/Mac in a different terminal run 'nc localhost 3000'."
-    -- putStrLn "Note: Running nc on the same server/Mac is only for the convenience of using 'localhost' instead of finding the proper IP address. It still demonstrates a proper network/socket connection. While the the Network.Socket package should work on Windows, nc seems to struggle to connect to Windows sockets."
-    -- putStrLn ""
-    addr <- resolve
-    -- putStrLn "Resolved AddrInfo struct"
-    E.bracket (open addr) close acceptConn
+  -- putStrLn "!!! To test proof of concept: !!!"
+  -- putStrLn "Run on the CS department Linux servers or a Mac, and on the same server/Mac in a different terminal run 'nc localhost 3000'."
+  -- putStrLn "Note: Running nc on the same server/Mac is only for the convenience of using 'localhost' instead of finding the proper IP address. It still demonstrates a proper network/socket connection. While the the Network.Socket package should work on Windows, nc seems to struggle to connect to Windows sockets."
+  -- putStrLn ""
+  addr <- resolve
+  -- putStrLn "Resolved AddrInfo struct"
+  E.bracket (open addr) close acceptConn
 
 createServer :: IO ()
-creaetServe = createSocket
+createServer = createSocket
