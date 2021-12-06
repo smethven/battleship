67ca4a5938d610eeb0741aea1ec525e3cf83e6e2
ruchitp <ruchitpalrecha@hotmail.com>
2021-12-05 16:14:32 -0800

    Have basic communication between server and client

--- haskell/src/Server.hs
+++ haskell/src/Server.hs
@@ -1,11 +1,39 @@
+-- {-# LANGUAGE OverloadedStrings #-}
+
 module Server (createServer) where
 
 import qualified Control.Exception as E
 import qualified Data.ByteString.Char8 as C
 import Network.Socket
 import Network.Socket.ByteString (recv, sendAll)
+import System.IO (getLine)
 
 {- server socket creation adapted from https://hackage.haskell.org/package/network-3.1.2.5/docs/Network-Socket.html -}
+
+runServer :: Socket -> IO ()
+runServer s = do
+  sendAll s (C.pack "Welcome to Battleship!\n")
+  C.putStrLn (C.pack "Welcome to Battleship!\n")
+  --   Write any other game init stuff here
+  runGame s
+
+runGame :: Socket -> IO ()
+runGame s = do
+  -- Assuming that the server goes first
+  getServerInput s
+  getClientInput s
+  runGame s
+
+getServerInput :: Socket -> IO ()
+getServerInput s = do
+  msg <- getLine
+  sendAll s (C.pack (msg ++ "\n"))
+
+getClientInput :: Socket -> IO ()
+getClientInput s = do
+  msg <- recv s 1024
+  --   putStr "Received: "
+  C.putStr msg
 
 resolve :: IO AddrInfo
 resolve = do
@@ -36,8 +64,8 @@ acceptConn sock =
     (accept sock)
     (close . fst)
     ( \(connection, _peer) -> do
-        -- putStrLn "Connected to client!"
-        sendAll connection (C.pack "Hello World")
+        putStrLn "Connected to client!"
+        runServer connection
     )
 
 createSocket :: IO ()
